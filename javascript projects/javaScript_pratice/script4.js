7f43c5129a57ee0f77614c5dd524f6259e2c9650
java_code <kmouryap@gmail.com>
2024-06-19 20:30:58 +0530

    javascript pratise

--- javascript projects/javaScript_pratice/script4.js
+++ javascript projects/javaScript_pratice/script4.js
@@ -17,5 +17,37 @@ console.log(age);
 
 //javascript class
 
+function Person1(name ,age)
+{
+    this.name = name;
+    this.age = age;
+}
+1
+const person2 = new Person1("elon musk" , 58);
 
+console.log(person2);
+
+class Person{
+    constructor(name ,age)
+    {
+        this.name =name;
+        this.age = age;
+    }
+    greet()
+    {
+        return "hello "+ this.name;
+    }
+    changename(newname){
+        this.name = newname;
+    }
+}
+
+const person1 = new Person("elon musk ", 56);
+
+
+console.log(person1.greet());
+person1.changename("mourya kumar");
+console.log(person1.name);
+
+//getter and setter
 
